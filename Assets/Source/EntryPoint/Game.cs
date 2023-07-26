52955ab097f402c27da59c51deca287674ebbb67
AtennopVer <anton7185569@gmail.com>
2023-07-26 21:14:09 +0300

    Added Game.cs

---
+++ Assets/Source/EntryPoint/Game.cs
@@ -0,0 +1,36 @@
+﻿using Leopotam.EcsLite;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Shooter.EntryPoint
+{
+    public sealed class Game : SerializedMonoBehaviour
+    {
+        private EcsWorld _ecsWorld;
+        private IEcsSystems _ecsSystems;
+        private IEcsSystems _fixedEcsSystems;
+        
+        private void Awake()
+        {
+            _ecsWorld = new EcsWorld();
+            _ecsSystems = new EcsSystems(_ecsWorld);
+            _fixedEcsSystems = new EcsSystems(_ecsWorld);
+
+            _ecsSystems.Init();
+            _fixedEcsSystems.Init();
+        }
+
+        private void Update()
+            => _ecsSystems.Run();
+
+        private void FixedUpdate()
+            => _fixedEcsSystems.Run();
+
+        private void OnDestroy()
+        {
+            _ecsSystems.Destroy();
+            _fixedEcsSystems.Destroy();
+            _ecsWorld.Destroy();
+        }
+    }
+}
